1e858bb693bb5f3ae730f1d60ee5363a53ec52c5
musishere <mustafabukhari333@gmail.com>
2026-04-15 13:56:44 +0500

    Refactor main function to improve readability and add temperature parameter for chat completion

--- invoke-llm/app.js
+++ invoke-llm/app.js
@@ -1,27 +1,31 @@
 import Groq from "groq-sdk";
 import { configDotenv } from "dotenv";
-configDotenv()
+configDotenv();
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-async function main(){
-    const completion = await groq.chat.completions.create({
-        model:"llama-3.3-70b-versatile",
-        messages:[
-            {
-                role:'system',
-                content:'You are Jarvis, a smart review grader. Your task is to analyse given review and return the senitment. Classify the review as positive, neutral or negative. Output must be in single word'
-            },
-            {
-                role:'user',
-                content:`Review: These headphones arrived quickly and looks great, but the left earcup stopped working after a week.
-                sentiment:`
+async function main() {
+  const completion = await groq.chat.completions.create({
+    temperature: 1,
+    // stop: "neg", //negative
+    // max_completion_tokens: 1000,
+    // fr equency_penalty: 2,
+    model: "llama-3.3-70b-versatile",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are Jarvis, a smart review grader. Your task is to analyse given review and return the senitment. Classify the review as positive, neutral or negative. Output must be in single word",
+      },
+      {
+        role: "user",
+        content: `Review: These headphones arrived quickly and looks great, but the left earcup stopped working after a week.
+                sentiment:`,
+      },
+    ],
+  });
 
-            }
-        ]
-    })
-
-    console.log({"Response":completion.choices[0].message.content})
+  console.log({ Response: completion.choices[0].message.content });
 }
 
 main();
