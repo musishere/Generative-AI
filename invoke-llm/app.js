d00a563dbb2cd0bbeddab6feaf613b9e2dc4606a
musishere <mustafabukhari333@gmail.com>
2026-04-15 12:47:50 +0500

    Add dotenv and groq-sdk dependencies; implement main function for API interaction

--- invoke-llm/app.js
+++ invoke-llm/app.js
@@ -1,1 +1,21 @@
-import express from "express"
+import Groq from "groq-sdk";
+import { configDotenv } from "dotenv";
+configDotenv()
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function main(){
+    const completion = await groq.chat.completions.create({
+        model:"llama-3.3-70b-versatile",
+        messages:[
+            {
+                role:'user',
+                content:'Hi'
+            }
+        ]
+    })
+
+    console.log({"Response from api":completion})
+}
+
+main()
